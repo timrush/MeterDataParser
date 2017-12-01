8c5c81e9e083172318a7a121f92da71d115438a3
trush_000 <trush_000@WINDOWS-KPPIH2E>
2017-12-01 14:40:30 -0500

    Added file loop

--- src/com/dsd/parser/SSNReadParser.java
+++ src/com/dsd/parser/SSNReadParser.java
@@ -28,107 +28,87 @@ public class SSNReadParser {
 
 	public static void main(String[] args) {
 
-	 try {
+	try {
 		 	
-		 String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-		 String appConfigPath = rootPath + "applicaton.properties";
+		String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+		String appConfigPath = rootPath + "applicaton.properties";
 
-		 InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");		 
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");		 
 		 
-		 Properties appProps = new Properties();
-		 appProps.load(input);
-		 String path = appProps.getProperty("winpath");
-		 System.out.println(path);
+		Properties appProps = new Properties();
+		appProps.load(input);
+		String path = appProps.getProperty("filepath");
+		String fileName = appProps.getProperty("filename");
+		 
+		File file = null;
+		int filesToProcess= Integer.parseInt(appProps.getProperty("numFilesToProcess"));
+		for (int i=0; i<= filesToProcess; i++) {
 
-		 String[] myFilesWin = new String[]{"C:\\work\\datafiles\\2012intervals1.xml",
-					"C:\\work\\datafiles\\2012intervals2.xml",
-					"C:\\work\\datafiles\\2012alldata.xml", 
-					"C:\\work\\datafiles\\onereg.xml",
-					"C:\\work\\datafiles\\2012xsdorig.xml"};
+			String fname = path + fileName + Integer.toString(i) + ".xml";
+			file = new File(fname);
+			System.out.println("Process file: "+ file.toString());
 
-		 String[] myFiles = new String[]{	"/media/trush/ex2part/datafiles/2012intervals1.xml",
-				 							"/media/trush/ex2part/datafiles/2012intervals2.xml",
-				 							"/media/trush/ex2part/datafiles/2012alldata.xml", 
-				 							"/media/trush/ex2part/datafiles/onereg.xml",
-				 							"/media/trush/ex2part/datafiles/2012xsdorig.xml"};
-		 File file = new File(myFilesWin[2]);
-		 System.out.println("Process file: "+ file.toString());
-
-		 JAXBContext jaxbContext = JAXBContext.newInstance(SSNExportDocument.class);
-
+			JAXBContext jaxbContext = JAXBContext.newInstance(SSNExportDocument.class);
+	
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 			SSNExportDocument xmlDoc = (SSNExportDocument) jaxbUnmarshaller.unmarshal(file);
-
+	
 			List<MeterData> meters = xmlDoc.getMeterData();					
-			
-
-	        try {
+	
+			try {
 				MeterDeviceStore mdStore = new MeterDeviceStore();
 				mdStore.getConnection();
 				
-	            for (MeterData m : meters) {
-			       
-	            	mdStore.insertDevice(m.meterName, m.utilDeviceID, m.macID);
-	    			System.out.println("Processing Meter: " + m.meterName);
+				for (MeterData m : meters) {
+					mdStore.insertDevice(m.meterName, m.utilDeviceID, m.macID);
+					System.out.println("Processing Meter: " + m.meterName);
 	            	
-	    			int storeData = 0;
-	    			
-	    			if( storeData > 0 )
-	    			{
-		            	if( m.getRegisterData() != null){
-		            		List<RegisterRead> regReads = m.getRegisterData().getRegisterRead();
-		            		for (RegisterRead rr : regReads)
-		            		{
-		            			List<Tier> tier = rr.tier;
-		            			for (Tier t : tier)
-			            		{
-		            				List<Register> register = t.register;
-		            				for (Register r : register)
-				            		{
-		            					// System.out.println("process register: " + r.getNumber());
-		            					mdStore.insertRegisterRead(rr.readTime, m.meterName, t.getNumber(), r);
+					int storeData = 0;
+					if( storeData > 0 ){
+						if( m.getRegisterData() != null){
+							List<RegisterRead> regReads = m.getRegisterData().getRegisterRead();
+			            	for (RegisterRead rr : regReads) {
+			            			List<Tier> tier = rr.tier;
+			            			for (Tier t : tier){
+			            				List<Register> register = t.register;
+			            				for (Register r : register){
+			            					mdStore.insertRegisterRead(rr.readTime, m.meterName, t.getNumber(), r);
+					            		}
 				            		}
 			            		}
-		            		}
-	            		}
-		            	
-		            	
-		            	if( m.getIntervalReadData()!=null){
-				    	   // System.out.println("Meter ID: " + m.meterName + " Interval reads: " + m.getIntervalReadData().numberIntervals);
-				    	   IntervalReadData intervalRD = m.getIntervalReadData();
-				    	   List<Interval> interval = intervalRD.getInterval();
-				    	   for ( Interval intervalInstance : interval){
+							}
+			            if( m.getIntervalReadData()!=null){
+			            	IntervalReadData intervalRD = m.getIntervalReadData();
+			            	List<Interval> interval = intervalRD.getInterval();
+			            	for ( Interval intervalInstance : interval){
 			    			   
-				    		   List<Reading> intervalReadings = intervalInstance.reading;
-				    		   for( Reading intRead : intervalReadings){
-				    			   //System.out.println("Interval: " + intervalInstance.getBlockSequenceNumber() + " " + intervalInstance.getIntervalSequenceNumber() + "Reading: " + intRead.channel + " " + intRead.value);
-				    			   mdStore.insertIntervalRead(m.meterName, intervalInstance.endTime, intervalInstance.getBlockSequenceNumber(), intervalInstance.getIntervalSequenceNumber(), intRead);
-				    					
-				    		   }
-	   			    	   }
+			            		List<Reading> intervalReadings = intervalInstance.reading;
+			            		for( Reading intRead : intervalReadings){
+			            			//System.out.println("Interval: " + intervalInstance.getBlockSequenceNumber() + " " + intervalInstance.getIntervalSequenceNumber() + "Reading: " + intRead.channel + " " + intRead.value);
+			            			mdStore.insertIntervalRead(m.meterName, intervalInstance.endTime, intervalInstance.getBlockSequenceNumber(), intervalInstance.getIntervalSequenceNumber(), intRead);
+			            		}
+			            	}
+			            }
+			            	
+			            if( m.getEventData() != null){
+			            	List<Event> events = m.getEventData().event;
+			            	for (Event evt : events){
+			            		System.out.println("Events: " + evt.eventName + " " + evt.value);
+			            		mdStore.insertEvent(m.meterName, evt.eventTime, evt.eventName, evt.value);
+			            	}
 		            	}
-		            	
-		            	if( m.getEventData() != null){
-	   			    	   List<Event> events = m.getEventData().event;
-				    	   for (Event evt : events){
-				    		   System.out.println("Events: " + evt.eventName + " " + evt.value);
-				    		   mdStore.insertEvent(m.meterName, evt.eventTime, evt.eventName, evt.value);
-				    	   }
-		            	}
-	    			}
-	            }
-	            mdStore.cleanup();	// Close DB connection
-
-	        	
-	        } catch (SQLException ex) {
-	            ex.printStackTrace();
-	        }
-		} catch (JAXBException e) {
-			e.printStackTrace();
+					}
+				}
+				mdStore.cleanup();	// Close DB connection
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
 		}
-		catch (IOException ei) {
-			ei.printStackTrace();
-		}
+	}catch (JAXBException e) {
+		e.printStackTrace();
 	}
-
+	catch (IOException ei) {
+		ei.printStackTrace();
+	}
+}
 }
